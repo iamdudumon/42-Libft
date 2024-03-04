80ffa41ea3e5952f29331ebc6a3cd08c97a43c51
Duhyun Kim <dukim@c2r3s7.42gyeongsan.kr>
2024-03-04 16:18:32 +0900

    feat: libft.h 함수 선언 추가

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: dukim <dukim@student.42gyeongsan.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 16:16:01 by dukim             #+#    #+#             */
-/*   Updated: 2024/03/04 13:44:02 by dukim            ###   ########.fr       */
+/*   Updated: 2024/03/04 15:15:50 by dukim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,10 @@
 
 # include <stdlib.h>
 
-int	ft_isalpha(char c);
-int	ft_isdigit(int c);
+int		ft_isalpha(char c);
+int		ft_isdigit(int c);
+size_t	ft_strlen(const char *s);
 void	*ft_memset(void *s, int c, size_t n);
-void	*ft_memcpy(void *dest, const void *src, size_t);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
 
 #endif
