f3b07c524b7ce97354785e80c50b23f385563bf1
Duhyun Kim <dukim@c2r3s7.42gyeongsan.kr>
2024-03-04 14:42:57 +0900

    feat: part1 function-3

---
+++ ft_strdup.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dukim <dukim@student.42gyeongsan.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/04 14:21:55 by dukim             #+#    #+#             */
+/*   Updated: 2024/03/04 14:42:19 by dukim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*strdup(const char *s)
+{
+	char	*dup;
+	size_t	len;
+
+	len = ft_strlen(s);
+	dup = (char *)malloc(sizeof(char) * (len + 1));
+	if (!dup)
+		return (0);
+	dup = (char *)ft_memcpy(dup, s, len);
+	dup[len] = '\0';
+	return (dup);
+}
