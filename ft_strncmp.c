b2d6bf549784d2ac0fa6a742ecef4e41afc09aa2
Duhyun Kim <dukim@c2r11s1.42gyeongsan.kr>
2024-03-08 18:59:09 +0900

    fix: unsigned char형으로 변환 후 비교

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,26 +6,32 @@
 /*   By: dukim <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 19:55:46 by dukim             #+#    #+#             */
-/*   Updated: 2024/02/29 02:24:47 by dukim            ###   ########.fr       */
+/*   Updated: 2024/03/08 18:58:30 by dukim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_strncmp(const char *s1, char *s2, size_t n)
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	len;
+	size_t			len;
+	unsigned char	c1;
+	unsigned char	c2;
 
 	len = 0;
-	while ((s1[len] != '\0' && s2[len] != '\0') && len < n)
+	c1 = (unsigned char)s1[len];
+	c2 = (unsigned char)s2[len];
+	while ((c1 != '\0' && c2 != '\0') && len < n)
 	{
-		if (s1[len] > s2[len])
-			return (s1[len] - s2[len]);
-		else if (s1[len] < s2[len])
-			return (s1[len] - s2[len]);
+		if (c1 != c2)
+			return (c1 - c2);
 		len++;
+		c1 = (unsigned char)s1[len];
+		c2 = (unsigned char)s2[len];
 	}
-	return (s1[len] - s2[len]);
+	if (len == n)
+		return (0);
+	return (c1 - c2);
 }
 /*
 #include <stdio.h>
@@ -46,6 +52,8 @@ int	main()
 	printf("%s <-> %s: %d\n", str5, str6, ft_strncmp(str5, str6, 2));
 	printf("%s <-> %s: %d\n", str7, str8, ft_strncmp(str7, str8, 3));
 	printf("%s <-> %s: %d\n", str3, str4, ft_strncmp(str3, str4, 0));
+	printf("%d\n", ft_strncmp("test\200", "test\0", 6));
+	printf("%d\n", (unsigned char)'\200');
 	
 	printf("\n");
 
